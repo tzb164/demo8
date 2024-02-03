74bf66ad5bae3bd7e3e2b1e8363fc6dcda9b6392
Tom Boesch <Thomas.Boesch@cms.hhs.gov>
2024-02-03 04:57:49 -0600

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ps.m3.demo8
+
+go 1.19
+
+require github.com/pioz/faker v1.7.3
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pioz/faker"
+)
+
+func main() {
+	fmt.Println("Random Colors Now: ", faker.ColorName())
+	fmt.Println("Random Colors Now: ", faker.ColorName())
+}
